4af7ddafd588dd6ce8baa2f6e56501fcf266dc1d
PedroSales117 <compasso.pedro.sales@contabilizei.com.br>
2023-08-24 12:33:40 -0300

    feat: add not found error and ok returns to service

--- src/modules/monthly/query/monthly-query.service.ts
+++ src/modules/monthly/query/monthly-query.service.ts
@@ -1,17 +1,42 @@
+import { Result, ok, err } from "neverthrow";
 import { database_model } from "../../shared/database/dto/database.dto";
 import { monthlyEstimatesSchema } from "../../shared/monthly/schema/monthly-create.schema";
 import { MonthlyEstimatesDTO } from "../../shared/dto/monthly.dto";
 import { MonthlyQueryEstimateDTO } from "./dto/monthly-query.dto";
+import {
+  IEstimateNotFoundReplyDTO,
+  IEstimateOkReplyDTO,
+} from "../../shared/dto/monthly-reply.dto";
 
-export class MonthlyEstimateQuery {
-  public async read(query: MonthlyQueryEstimateDTO): Promise<unknown> {
-    const monthly_estimate = database_model<MonthlyEstimatesDTO>(
-      "meses",
-      monthlyEstimatesSchema,
-    );
+const read = async (
+  query: MonthlyQueryEstimateDTO,
+): Promise<Result<IEstimateOkReplyDTO, IEstimateNotFoundReplyDTO>> => {
+  const monthly_estimate = database_model<MonthlyEstimatesDTO>(
+    "meses",
+    monthlyEstimatesSchema,
+  );
 
-    return new monthly_estimate().collection.findOne({
-      [query.field]: query.value,
+  const estimate = await new monthly_estimate().collection.findOne({
+    [query.field]: query.value,
+  });
+
+  if (estimate === null) {
+    return err({
+      status: 404,
+      data: {
+        error: "Couldn't find monthly estimate",
+      },
     });
   }
-}
+
+  return ok({
+    status: 200,
+    data: {
+      estimate,
+    },
+  });
+};
+
+export default {
+  read,
+};
